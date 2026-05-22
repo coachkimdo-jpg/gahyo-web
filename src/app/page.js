2431c67345e174e3122eea30af9113be4cb65c17
Gahyo Admin <gahyofuneral@naver.com>
2026-05-22 18:57:10 +0900

    구글 사이트 이름 노출 강화를 위한 WebSite JSON-LD alternateName 추가 및 도메인 수정

--- src/app/page.js
+++ src/app/page.js
@@ -137,7 +137,8 @@ export default function HomePage() {
       '@context': 'https://schema.org',
       '@type': 'WebSite',
       'name': '가효상조',
-      'url': 'https://gahyo.com',
+      'alternateName': ['가효상조 장례식장', 'gahyo.co.kr'],
+      'url': 'https://gahyo.co.kr/',
       'description': '100% 후불제 상조 및 투명한 장례 서비스'
     },
     {
