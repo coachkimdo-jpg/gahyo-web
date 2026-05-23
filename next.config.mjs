42b98afb7546da4ae8dae98cb822d3ae1def776e
Gahyo Admin <gahyofuneral@naver.com>
2026-05-23 21:12:08 +0900

    Add clickjacking mitigation security headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   productionBrowserSourceMaps: true,
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self';",
+          }
+        ],
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
