559166785696bd03492a6e9b846fe30574fc9652
Gahyo Admin <gahyofuneral@naver.com>
2026-05-23 17:44:32 +0900

    perf: dynamically import components to reduce main thread work

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,12 @@
 import './globals.css';
 import Header from '@/components/Header';
-import Footer from '@/components/Footer';
-import EmergencyFloat from '@/components/EmergencyFloat';
+import dynamic from 'next/dynamic';
+
+const Footer = dynamic(() => import('@/components/Footer'), { ssr: true });
 import { GoogleAnalytics } from '@next/third-parties/google';
 import localFont from 'next/font/local';
+
+const EmergencyFloat = dynamic(() => import('@/components/EmergencyFloat'), { ssr: false });
 
 const pretendard = localFont({
   src: './fonts/PretendardVariable.woff2',
